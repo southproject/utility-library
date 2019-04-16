f3dd3d0998e6a6b0fac6ffe38a21dc7fe6f2c437
southproject <yukipedia_yui@yahoo.com>
2019-04-17 01:16:13 +0800

    pipe lazy

--- Process/lazyLoad.js
+++ Process/lazyLoad.js
@@ -73,7 +73,7 @@ const map = function (flow, transform) {
 
   const nums = join(take(filter(map(range(0, 20), n => n * 10), n => n % 3 === 0), 2));
   console.log(nums); */
-  const range = function* (from, to) {
+  /* const range = function* (from, to) {
     for(let i = from; i < to; i++) {
       console.log('range\t', i);
       yield i;
@@ -91,12 +91,12 @@ const map = function (flow, transform) {
     for(const data of flow) {
       console.log('filter\t', data);
       if (condition(data)) {
-        yield data;
+        yield data; //有条件的yield促使下级条件执行
       }
     }
   }
   
-  const stop = function*(flow, condition) {
+  const stop = function* (flow, condition) {
     for(const data of flow) {
       yield data;
       if (condition(data)) {
@@ -116,4 +116,117 @@ const map = function (flow, transform) {
   
    for(const n of take(filter(map(range(0, 10000), n => n * 10), n => n % 3 === 0), 2)) {
      console.log('num:\t', n, '\n');
-   }
+   } */
+
+//非生成器实现没有太大价值：采取每次执行下游函数时，上游函数的执行部分（flow()）卡在下游函数的执行过程中，导致回溯到源头实现了所谓的
+//“直到执行时才顺序”lazyFn而不是“执行时先得出上游函数值作为输入”，扩展性不强
+
+
+/* const Lazy = iterator => {
+    const next = iterator.next.bind(iterator)
+      const map = () => {}
+      const filter = () => {}
+      const takeWhile = () => {}
+      return {
+        next,
+        map,
+        filter,
+        takeWhile,
+      }
+    }
+      const map = f => {
+        const modifiedNext = () => {
+          const item = next()
+          const mappedValue = f(item.value)
+          return {
+            value: mappedValue,
+            done: item.done,
+          }
+        }
+        const newIter = { ...iterator, next: modifiedNext }
+        return Lazy(newIter)
+      }
+      const filter = predicate => {
+        const modifiedNext = () => {
+          while (true) {
+            const item = next()
+            if (predicate(item.value)) {
+              return item
+            }
+          }
+        }
+        const newIter = { ...iterator, next: modifiedNext }
+        return Lazy(newIter)
+      }
+
+      const takeWhile = predicate => {
+        const result = []
+        let value = next().value
+        while (predicate(value)) {
+          result.push(value)
+          value = next().value
+        }
+        return result
+      }
+ */
+      const Lazy = iterator => {
+        const next = iterator.next.bind(iterator)
+      
+        const map = f => {
+          const modifiedNext = () => {
+            const item = next()
+            const mappedValue = f(item.value)
+            return {
+              value: mappedValue,
+              done: item.done,
+            }
+          }
+          const newIter = { ...iterator, next: modifiedNext }
+          return Lazy(newIter)
+        }
+      
+        const filter = predicate => {
+          const modifiedNext = () => {
+            while (true) {
+              const item = next()
+              if (predicate(item.value)) {
+                return item
+              }
+            }
+          }
+          const newIter = { ...iterator, next: modifiedNext }
+          return Lazy(newIter)
+        }
+      
+        const takeWhile = predicate => {
+          const result = []
+          let value = next().value
+          while (predicate(value)) {
+            result.push(value)
+            value = next().value
+          }
+          return result
+        }
+      
+        return Object.freeze({
+          map,
+          filter,
+          takeWhile,
+          next,
+        })
+      }
+      
+      const numbers = function*() {
+        let i = 1
+        while (true) {
+          yield i++
+        }
+      }
+
+      console.log(Lazy(numbers())
+      .map(x => x ** 2)
+      .filter(x => x % 2 === 1)
+      .takeWhile(x => x < 10000)
+      .reduce((x, y) => x + y))
+    // => 16650
+//链式写法固然容易理解一个值依次执行了各项操作，但是能够不停调用next也是依靠闭包（上游中的函数modifynext传给下游）
