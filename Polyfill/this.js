ac9e4bc3f6435eb065dd5e8a4b14304da4fbdc8e
southproject <yukipedia_yui@yahoo.com>
2019-04-30 10:10:07 +0800

    bind selfdefinition

--- Polyfill/this.js
+++ Polyfill/this.js
@@ -83,3 +83,21 @@ Function.prototype.apply2 = function(context = window) {
     delete context.fn()
     return result
 }
+
+Function.prototype.bind2 = function(content) {
+    if(typeof this != "function") {
+        throw Error("not a function")
+    }
+    // 若没问参数类型则从这开始写
+    let fn = this;
+    let args = [...arguments].slice(1);
+
+    let resFn = function() {
+        return fn.apply(this instanceof resFn ? this : content,args.concat(...arguments) )
+    }
+    function tmp() {}
+    tmp.prototype = this.prototype;
+    resFn.prototype = new tmp();
+
+    return resFn;
+}
