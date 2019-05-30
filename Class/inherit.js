b34dbab8787c7e624e1669436732ff225fadf1c9
southproject <yukipedia_yui@yahoo.com>
2019-05-30 23:46:49 +0800

    instaceof

--- Class/inherit.js
+++ Class/inherit.js
@@ -29,3 +29,14 @@ export function mixin(target, source, overlay) {
 
     defaults(target, source, overlay);
 }
+
+export function instanceOf(left,right) {
+    
+        let proto = left.__proto__;
+        let prototype = right.prototype
+        while(true) {
+            if(proto === null) return false
+            if(proto === prototype) return true
+            proto = proto.__proto__;
+        }
+    }
