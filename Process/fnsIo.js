7e178be07ab0e586660f6bea6fdc801079806c14
southproject <yukipedia_yui@yahoo.com>
2019-05-31 21:27:45 +0800

    增加uncurry

--- Process/fnsIo.js
+++ Process/fnsIo.js
@@ -44,3 +44,15 @@ const mapp = (arrs,fn)=>arrs.reduce((acc,x) => (acc.concat([fn(x)])),[])
 //[].contact(fn(a)) Function.prototype.concat.apply(acc,fn(x))
 //cautious about return value of push、map、concat
 export {pipe,compose,simpel_curry,mapp}
+
+
+const uncurry = (fn, n = 1) => (...args) => {
+    const next = acc => args => args.reduce((x, y) => x(y), acc);
+    if (n > args.length) throw new RangeError('Arguments too few!');
+    return next(fn)(args.slice(0, n));
+  };
+
+
+  const add = x => y => z => x + y + z;
+  const uncurriedAdd = uncurry(add, 3);
+  uncurriedAdd(1, 2, 3); // 6
